7b47de288a570ba0517cc2e55d33388ff0af66aa
missto825 <suhwan825@gmail.com>
2021-07-03 22:48:21 +0900

    1만시간 js조금. 얼마 하지 않음

--- Shool/infern/Basic Resouce/js/10000hours.js
+++ Shool/infern/Basic Resouce/js/10000hours.js
@@ -1,1 +1,6 @@
-// js file
+const startButton = document.querySelector(".start_btn");
+
+shareButton.addEventLisener('click',copyUrl)
+openButton.addEventLisener('click',openUrl)
+closeButton.addEventLisener('click',closeUrl)
+startButton.addEventLisener('click',calculatorUrl)
