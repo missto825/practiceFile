66ea3cbf49f576ef49aaa0946910215920d95bf6
missto825 <suhwan825@gmail.com>
2021-07-07 22:56:16 +0900

    슬라이드 따라하기 1일차
    아직 기초도 안되서 힘들다

---
+++ cpp/HelloWorld.asm
@@ -0,0 +1,15 @@
+%include "io64.inc"
+
+section .text
+global CMAIN
+CMAIN:
+    ;write your code here
+    
+    PRINT_STRING msg
+    
+    
+    xor rax, rax
+    ret
+    
+section .data
+    msg db 'hello world', 0x00
