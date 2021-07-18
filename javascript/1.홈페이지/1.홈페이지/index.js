2476194a903cf715f88ac7e975e4b5b17321e08d
missto825 <68280929+missto825@users.noreply.github.com>
2021-07-18 23:22:47 +0900

    transition 습득.커서 움직임에서 자연슬러운 배경 움직임

--- javascript/1.홈페이지/1.홈페이지/index.js
+++ javascript/1.홈페이지/1.홈페이지/index.js
@@ -1,15 +1,32 @@
+let x = 0;
+let y = 0;
+let cursor_item;
+let mx = 0;
+let my = 0;
+let mspeed = 0.01;
 window.onload = function(){
     let h1 = document.getElementsByTagName("h1")[0];
-    let cursor_item = document.getElementsByClassName("cursor_item")[0];
+    cursor_item = document.getElementsByClassName("cursor_item")[0];
     window.addEventListener("mousemove", mouseFunc,false);
 
     function mouseFunc(e){
-        h1.innerHTML = "x : " + e.clientX + "  y : " + e.clientY;
-        cursor_item.style.transform = "translate("+e.clientX +"px," + e.clientY + "px)";
-        //console.log(e.clientX);
+        x = e.clientX;
+        y = e.clientY;
+        
+    h1.innerHTML = "x : " + x + "  mx : " + mx;
     }
+    loop();
 }
 
+function loop(){
+    mx += (x - mx) * mspeed;
+    my += (y - my) * mspeed;
+
+    
+
+    cursor_item.style.transform = "translate("+mx +"px," + my + "px)";
+    window.requestAnimationFrame(loop);
+}
 //addEventListener() = 유니티의 이벤트 트리거, 특정한 행동을 포착하면 특정 함수에 보낼수있음
 //("타입",보낼함수,???);
 //없어도 되네 왜 적은거지
