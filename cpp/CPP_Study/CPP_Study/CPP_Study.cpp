f29fd37716c5ae7c78812453da901d5f4274d434
missto825 <suhwan825@gmail.com>
2021-08-03 20:01:46 +0900

    학교

--- cpp/CPP_Study/CPP_Study/CPP_Study.cpp
+++ cpp/CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
+using namespace std;
 
+char ch = 'a';
+// UTF8 UTF16
+// UTF8은 영어,숫자 1바이트 유럽문자 2바이트 한국어 중국어 3바이트
+// UTF16은 대부분 2바이트
+
+wchar_t wch = L'안'; //2바이트가 됨
+
+// Escape Sequence
+// 표시하기 애매한 애들을 표현
+// \t = 아스키코드 9 TAB
+// \n = 아스키코드 10 LineFeed (한줄 아래로(
 int main()
 {
-    int num = 4;
-    std::cout << "Hello World!\n";
+    cout << wch << endl;
+    //cout은 char전용 한국어를 암시해줘야함
+    wcout.imbue(locale("kor"));
+    wcout << wch << endl;
+    cout << "Hello World!" << endl;
 }
